33feb624e9318b7cf6400c33121b4b96566b4224
manylov <yellowmanylov@gmail.com>
2022-04-19 16:18:51 +0700

    fix for liner

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { getName } from '../src/cli.js';
-// greeting
 console.log('Welcome to the Brain Games!');
 const name = getName();
 console.log(`Hello, ${name}!`);
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
 import { gameLogic } from '../src/index.js';
+
 import { brainPrimeGame, gameRules } from '../games/prime.js';
 console.log(gameLogic(brainPrimeGame, gameRules));
