6a8b054d74ef93ed625d9a6f6907e7c15d4085f1
manylov <yellowmanylov@gmail.com>
2022-04-19 15:50:41 +0700

    fix gcd gamelogic

--- games/gcd.js
+++ games/gcd.js
@@ -4,7 +4,7 @@ export const brainGCDGame = () => {
     const arr = []
 
     const num1 = (Math.ceil(Math.random() * 20 + 2))
-    const num2 = (Math.ceil(Math.random() * 100))
+    const num2 = (Math.ceil(Math.random() * 50))
     arr.push(`${num1} ${num2}`)
     arr.push(String(gcd(num1, num2)))
 
