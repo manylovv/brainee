236160d34f9da4ab3e1cf83dc1daaf4479998d81
manylov <yellowmanylov@gmail.com>
2022-04-19 16:19:09 +0700

    fix for linter

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
 import { gameLogic } from '../src/index.js';
+
 import { brainProgressionGame, gameRules } from '../games/progression.js';
 console.log(gameLogic(brainProgressionGame, gameRules));
--- games/calc.js
+++ games/calc.js
@@ -1,25 +1,27 @@
 const getRandomOperator = () => {
-    const arrOperators = ['+', '-', '*'];
-    const random = Math.floor((Math.random() * arrOperators.length));
-    return arrOperators[random];
+  const arrOperators = ['+', '-', '*'];
+  const random = Math.floor((Math.random() * arrOperators.length));
+  return arrOperators[random];
 };
 export const gameRules = 'What is the result of the expression?';
 export const brainCalcGame = () => {
-    const arr = [];
-    const num1 = (Math.ceil(Math.random() * 15));
-    const num2 = (Math.ceil(Math.random() * 15));
-    const randomOperator = getRandomOperator();
-    arr.push(`${num1} ${randomOperator} ${num2}`);
-    switch (randomOperator) {
-        case "+":
-            arr.push(String(num1 + num2));
-            break;
-        case "-":
-            arr.push(String(num1 - num2));
-            break;
-        case "*":
-            arr.push(String(num1 * num2));
-            break;
-    }
-    return arr;
+  const arr = [];
+  const num1 = (Math.ceil(Math.random() * 15));
+  const num2 = (Math.ceil(Math.random() * 15));
+  const randomOperator = getRandomOperator();
+  arr.push(`${num1} ${randomOperator} ${num2}`);
+  switch (randomOperator) {
+    case "+":
+      arr.push(String(num1 + num2));
+      break;
+    case "-":
+      arr.push(String(num1 - num2));
+      break;
+    case "*":
+      arr.push(String(num1 * num2));
+      break;
+    default:
+      return null
+}
+  return arr;
 };
