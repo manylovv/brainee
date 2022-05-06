b010687c9423941e9e732ddec3c77368bed4baa2
manylov <yellowmanylov@gmail.com>
2022-05-06 14:05:36 +0700

    fix for linter

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,12 +3,12 @@ import runEngine from '../index.js';
 
 const isPrime = (number) => {
   if (isNaN(number) || !isFinite(number) || number % 1 || number < 2) return false;
-  const sqrt = Math.sqrt(number); //returns the square root of the passed value
-  for (var i = 2; i <= sqrt; i++)
-      if (number % i == 0) return false;
+  const sqrt = Math.sqrt(number);
+  for (var i = 2; i <= sqrt; i++) {
+    if (number % i == 0) return false;
+  }
   return true;
-}
-
+};
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const getBrainPrime = () => {
